67b583012982737c29a1698c611eb8c8bddf83bc
nethmina1028 <nethmina1028@gmail.com>
2025-06-04 12:15:24 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div>
+    <h1 className="text-3xl font-bold underline">
+      Welcome to Fimovate!
+    </h1>
+    <p className="mt-4 text-lg">
+      This is a sample application built with Next.js and Tailwind CSS.
+    </p>
+    
+ </div> 
+  );
+}

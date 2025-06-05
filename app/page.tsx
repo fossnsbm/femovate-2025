10e5bc763c984d1570de7333d4bbbaaa89d00710
ashini reshana nanayakkara <132426734+NanayakkaraAR@users.noreply.github.com>
2025-06-05 12:19:01 +0530

    called to the front

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Hero from "@/app/sections/hero";
+import Femovate from "@/app/sections/femovate";
 
 export default function Home() {
   return (
  <div>
     <Hero/>
+    <Femovate/>
+    
 
  </div> 
   );

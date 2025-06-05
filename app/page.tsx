bcadc7e3eb49fadec90690c95b651723bdd2d8ac
Hikarunathilake <hikarunathilake@students.nsbm.ac.lk>
2025-06-06 01:34:00 +0530

    contact us with resolved conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hero from "@/app/sections/hero";
 import Femovate from "@/app/sections/femovate";
 import Form from "@/app/sections/form";
-
+import ContactUs from "./sections/contactUs";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
     <Hero/>
     <Femovate/>
     <Form/>
-
+    <ContactUs/>
     
 
  </div> 

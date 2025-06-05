d19ea7a98d4b430af3fd483dab41d91912dcf58b
Hikarunathilake <hikarunathilake@students.nsbm.ac.lk>
2025-06-06 01:08:55 +0530

    contact us done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Hero from "@/app/sections/hero";
 import Femovate from "@/app/sections/femovate";
+import ContactUs from "./sections/contactUs";
 
 export default function Home() {
   return (
  <div>
     <Hero/>
     <Femovate/>
+   
+      <ContactUs />
+    
     
 
  </div> 

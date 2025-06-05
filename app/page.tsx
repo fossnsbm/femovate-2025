b5b64aa655a8840984db15082baa1f4699d69627
Yashira-De-Silva <yashiradesilva@gmail.com>
2025-06-06 01:04:37 +0530

    feat(9): navbar done with the dropdown list

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Hero from "@/app/sections/hero";
 import Femovate from "@/app/sections/femovate";
-
+import Navbar from "./sections/navbar";
 export default function Home() {
   return (
  <div>
+  <Navbar/>
     <Hero/>
     <Femovate/>
     

627ed9a7d6275330ee57e5436c5bd615ad7fa9b0
WA5aNA <wasanafernando2003@gmail.com>
2025-06-06 20:31:13 +0530

    feat(wip): implement FAQ Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Hero from "@/app/sections/hero";
 import Femovate from "@/app/sections/femovate";
+import FAQSection from "./sections/faq-section";
 
 export default function Home() {
   return (
  <div>
     <Hero/>
     <Femovate/>
-    
+    <FAQSection/>
 
  </div> 
   );
